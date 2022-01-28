4d3b6f7b89a7322d33970f7bbdbf3437df3c9f74
Mattis Erkensten <m.erkensten@icloud.com>
2022-01-28 12:18:40 +0100

    Påbörjat implementation av firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB0eeWwnTzdz6Rle0myr4FyXZmtGHRijEQ',
+  authDomain: 'cooking-ninja-site-53eed.firebaseapp.com',
+  projectId: 'cooking-ninja-site-53eed',
+  storageBucket: 'cooking-ninja-site-53eed.appspot.com',
+  messagingSenderId: '945481103282',
+  appId: '1:945481103282:web:6a85c94e121130fe33d6ab',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialzr Srervices
+const projectFirsestore = firebase.firestore();
+
+export { projectFirsestore };
